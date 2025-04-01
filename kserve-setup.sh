5caa4534b117d10b551b91259b5decdfaa11118b
David Martin <david-martin@users.noreply.github.com>
2025-04-01 12:31:07 +0100

    Update kserve-setup.sh
    Signed-off-by: David Martin <davmarti@redhat.com>

--- kserve-setup.sh
+++ kserve-setup.sh
@@ -102,7 +102,7 @@ if [ -z "${GATEWAY_HOST:-}" ]; then
 fi
 
 echo "Configuring KServe v0.15.0 to set 'enableGatewayApi'..."
-helm upgrade kserve oci://ghcr.io/kserve/charts/kserve --version v0.15.0-rc1 --namespace kserve --create-namespace --wait \
+helm upgrade kserve oci://ghcr.io/kserve/charts/kserve --version v0.15.0 --namespace kserve --create-namespace --wait \
   --set kserve.controller.gateway.ingressGateway.enableGatewayApi=true \
   --set kserve.controller.gateway.ingressGateway.kserveGateway=kserve/kserve-ingress-gateway \
   --set kserve.controller.deploymentMode=RawDeployment
